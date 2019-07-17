eafb551b5b8a85728df723355cd7c2437b4e601e
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-07-16 20:57:06 -0700

    setting up model schema

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist"
+);
 
 // Start the API server
 app.listen(PORT, function() {
