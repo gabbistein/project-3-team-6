92529420842c716c43dc4c813cd27d7102dc64de
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-07-18 20:00:29 -0700

    pull request

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -7,6 +7,7 @@ import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
 import { Input, TextArea, FormBtn } from "../components/Form";
 
+
 class Books extends Component {
   state = {
     books: [],
