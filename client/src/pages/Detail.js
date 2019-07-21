782df39ae165f271b853fb2d52bc6d37884df7c1
B Lub <brandonlublin@brandons-MacBook-Pro.local>
2019-07-21 12:08:00 -0700

    added fbapi data/url/apikey, removed books and detail.js from old boilerplate

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
-import API from "../utils/API";
+import API from "../utils/FBAPI";
 
 class Detail extends Component {
   state = {
