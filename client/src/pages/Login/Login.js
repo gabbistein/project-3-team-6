c7f2614035b2ed78887ccf684036e239603d38a7
gabbistein <gabbistein@outlook.com>
2019-07-25 21:45:25 -0700

    login page is really added!

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from "./node_modules/react";
+import React, { Component } from "react";
 import googleButton from './google_signin_buttons/web/1x/btn_google_signin_dark_normal_web.png'
 
 class Login extends Component {
