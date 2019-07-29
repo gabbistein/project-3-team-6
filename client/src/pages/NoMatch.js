f6612f9601bb82e25be383068176f42daa016b8a
gabbistein <gabbistein@outlook.com>
2019-07-28 18:11:01 -0700

    UI updates, jumbotrons everywhere, yay styles

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,23 +1,31 @@
 import React from "react";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
+import Nav from "../components/Nav";
+
+let noMatchStyle = {
+  marginTop: 90
+}
 
 function NoMatch() {
   return (
-    <Container fluid>
-      <Row>
-        <Col size="md-12">
-          <Jumbotron>
-            <h1>404 Page Not Found</h1>
-            <h1>
-              <span role="img" aria-label="Face With Rolling Eyes Emoji">
-                🙄
+    <div>
+      <Nav />
+      <div style={noMatchStyle}>
+        <Jumbotron>
+          <Row>
+            <Col size="md-12">
+              <h1>404: Page Not Found</h1>
+              <h1>
+                <span role="img" aria-label="Face With Rolling Eyes Emoji">
+                  👻
               </span>
-            </h1>
-          </Jumbotron>
-        </Col>
-      </Row>
-    </Container>
+              </h1>
+            </Col>
+          </Row>
+        </Jumbotron>
+      </div>
+    </div>
   );
 }
 
