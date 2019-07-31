35d9b4dce0c97d65494f31eb183b1b8adf9fd37f
B Lub <brandonlublin26@gmail.com>
2019-07-30 19:12:58 -0700

    added keys.js file for config data and removed merge conflicts from server that were affecting heroku

--- client/src/utils/tumblrapi.js
+++ client/src/utils/tumblrapi.js
@@ -1,7 +1,7 @@
 // Authenticate via API Key
 var tumblr = require("tumblr.js");
 var client = tumblr.createClient({
-  consumer_key: "svEF96HgjjM2N4CizV6cI9phVPpWRzKwl2ZNDpev5LwNomZZs0"
+  consumer_key: ""
 });
 let user = "picsthatmakeyougohmm.tumblr.com";
 // Make the request
