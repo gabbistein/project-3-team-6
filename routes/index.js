7964429adaa584712acf08b65f66e3a7a6029d22
B Lub <brandonlublin26@gmail.com>
2019-07-31 22:09:44 -0700

    adding passport tumblr strategy, callback is currently erroring but i think i need to fix my callback routing, will fix during class

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const passport = require("passport");
 
 // API Routes
 router.use("/api", apiRoutes);
+
+router.get("/auth/tumblr",
+  passport.authenticate("tumblr"));
+
+router.get("/auth/tumblr/callback", 
+  passport.authenticate("tumblr", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/");
+    console.log("this is for tumblr");
+    
+  });
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
