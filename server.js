4a112fc6b6969140b1404e2a93c575fb2b3eaa62
B Lub <brandonlublin26@gmail.com>
2019-08-05 20:56:16 -0700

    moved api call from singleContact/index into the server file. will eventually move to routes, but for now, i still need to make sure the data is moving from back to front

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/"));
+  app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
